79d3b1216b7a491baef2a43b36bee09cc2cded82
David Ngo and Renee Sarley <david@>
2018-05-29 09:46:08 -0700

    add additional js functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,32 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert(add(number1, number2));
+var multiply = function(number1, number2){
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// var calculateBMI = function(weight, height) {
+//   return weight / (height * height) ;
+// }
+//
+// var test;
+// var weight = parseInt(prompt("Weight in kg:"));
+// var height = parseInt(prompt("Height in m:"));
+//
+// alert(calculateBMI(weight, height));
+
+var convertTemp = function(tempC) {
+  return tempC * 1.8 + 32
+};
+
+var userTemp = parseInt(prompt("Enter a temperture in Celsius"));
+
+alert(convertTemp(userTemp));
